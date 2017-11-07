3ea434de34de7c4741929977f98b74278ab1d74e
danielmoralesp <daniel_moralesp@hotmail.com>
2017-11-07 18:02:06 -0500

    Config mongodb

--- sodibee/config/application.rb
+++ sodibee/config/application.rb
@@ -12,6 +12,11 @@ require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+#Mongo
+require 'mongoid'
+
+Mongoid.load!(File.expand_path('mongoid.yml', './config'))
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
