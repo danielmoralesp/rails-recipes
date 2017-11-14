83515dbd3d557df0b6709d027182b911c696ace1
danielmoralesp <daniel_moralesp@hotmail.com>
2017-11-14 13:09:40 -0500

    first steps with webrat

--- showtime-rspec-cucumber/db/schema.rb
+++ showtime-rspec-cucumber/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110154317) do
+ActiveRecord::Schema.define(version: 20171114164302) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.date     "showtime_date"
